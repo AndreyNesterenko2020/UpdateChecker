76735d7e21cb7d5de40f2b8db3d7e90123484fa0
AndreyNesterenko2020 <74727724+AndreyNesterenko2020@users.noreply.github.com>
2023-05-01 01:40:11 -0400

    Add files via upload

--- updateChecker.js
+++ updateChecker.js
@@ -1,10 +1,10 @@
-//update checker version 1.1, for discord webhooks
+//update checker version 1.2, for discord webhooks
 
 var request = require('request');
 var discord = require('discord.js');
 var configuration = {target: "https://venge.io/changes.txt", webhook: false, roleID: 0, delay: 1800000}
 try {
-  configuration = require('./config.js'); //config file must export the target url, the webhook url, the roleID, and optionally the time in ms between every check (delay)
+  configuration = require('./config.js'); //config file must export the target url, the webhook url, the roleID, and optionally the header (a constant string that is stripped away from the top of the file), or time in ms between every check (delay)
 } catch (e) {}
 
 var webhook = configuration.webhook && new discord.WebhookClient({url: configuration.webhook});
@@ -13,7 +13,13 @@ var difference = null;
 
 function get() {
   request(configuration.target, function (error, response, body) {
-    error && console.log(error);
+    if(error) {
+      console.log(error);
+      return;
+    }
+    if(configuration.header) {
+      body = body.split(configuration.header)[1];
+    }
     difference = body.split(lastValue)[0];
     lastValue != null && difference && fireWebhook("<@&"+configuration.roleID+"> Game has been updated!\n```"+difference+"```");
     lastValue = body;
